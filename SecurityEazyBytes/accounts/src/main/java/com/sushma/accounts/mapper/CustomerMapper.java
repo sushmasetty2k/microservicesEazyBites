3c8ff349f51238452e3a5af48b27e9f906597508
srisushmas <srisushma.setty@outlook.com>
2026-08-19 15:14:50 +0530

    Add SecurityEazyBytes

---
+++ SecurityEazyBytes/accounts/src/main/java/com/sushma/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,29 @@
+package com.sushma.accounts.mapper;
+
+import com.sushma.accounts.dto.CustomerDetailsDto;
+import com.sushma.accounts.dto.CustomerDto;
+import com.sushma.accounts.model.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
---
+++ SecurityEazyBytes/accounts/src/main/java/com/sushma/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,16 @@
+package com.sushma.accounts.service.client;
+
+import com.sushma.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "loans", fallback = LoansFallBack.class)
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader(value = "eazybank_correlation_id") String correlationId,
+                                                     @RequestParam String mobileNumber);
+}
